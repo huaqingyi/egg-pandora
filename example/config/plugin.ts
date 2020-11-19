2046e1051186355c3d1de73132acaaff0bb75197
huaqingyi <yihuaqing@talefun.com>
2020-11-19 20:13:17 +0800

    init

--- example/config/plugin.ts
+++ example/config/plugin.ts
@@ -1,12 +1,12 @@
 /*
  * @FilePath: /egg-pandora/example/config/plugin.ts
- * @Descripttion:
- * @version:
+ * @Descripttion: 
+ * @version: 
  * @Author: 易华青
- * @Date: 2020-11-16 10:46:55
+ * @Date: 2020-11-19 16:22:11
  * @LastEditors: huaqingyi
- * @LastEditTime: 2020-11-19 16:27:45
- * @debugger:
+ * @LastEditTime: 2020-11-19 20:12:58
+ * @debugger: 
  */
 import { EggPlugin } from 'egg';
 
